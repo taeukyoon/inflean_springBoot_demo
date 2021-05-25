20dc5de52f859cd523c6a8b0ec88e84b5ebe031b
taeuk <mee9203@naver.com>
2021-05-25 10:50:39 +0900

    add Service

---
+++ src/main/java/hello/hello_spring/Service/MemberService.java
@@ -0,0 +1,42 @@
+package hello.hello_spring.Service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+
+        memberRepository.findByName(member.getName())
+                //값 존재하면 지정된 문자를 보여준다.
+                .ifPresent(member1 -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
