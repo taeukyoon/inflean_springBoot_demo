a70862724ce8d57ecb19eb4ac251a0b0aa52fbf9
taeuk <mee9203@naver.com>
2021-05-26 10:48:10 +0900

    MemberList

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -4,9 +4,12 @@ import hello.hello_spring.Service.MemberService;
 import hello.hello_spring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -33,6 +36,12 @@ public class MemberController {
 
         //홈화면으로
         return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
 
